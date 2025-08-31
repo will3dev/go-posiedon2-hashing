f58cd63736c869d30f2132acf98ad8a414c50bbe
Will <willwaltrip@gmail.com>
2025-08-31 18:21:04 -0400

    Added new tests

--- pkg/poseidon2/helpers_test.go
+++ pkg/poseidon2/helpers_test.go
@@ -1,6 +1,7 @@
 package poseidon2
 
 import (
+	"log"
 	"math/big"
 	"testing"
 )
@@ -22,5 +23,45 @@ func TestAddFullRoundConstants(t *testing.T) {
 		if result[i].Cmp(exp) != 0 {
 			t.Errorf("Position %d: expected %v, got %v", i, exp, result[i])
 		}
+	}
+}
+
+func TestInternalMatrix(t *testing.T) {
+	p := big.NewInt(17) // Small prime for Testing
+	s := []int{2, 3, 4, 5}
+
+	// Test Rounds
+	for i := 0; i < len(s); i++ {
+		log.Printf("Matrix Size: %d x %d", s[i], s[i])
+
+		// Act
+		result := generateInternalMatrix(s[i], p)
+
+		// Assert
+		for j := 0; j < s[i]; j++ {
+			expected := new(big.Int).SetInt64(int64(j + 1))
+			// This is a diagonal matrix. Position value at i in Row i should be equal to i + 1
+			if result[j][j].Cmp(expected) != 0 {
+				t.Errorf("Row %d Position %d: expected %v, got %v", j, j, expected, result[j][j])
+			}
+		}
+
+	}
+
+}
+
+
+func TestConvertHex(t *testing.T) {
+	// Set up
+	i := "0x11" // 17 as hex
+	p := big.NewInt(19)
+	// Act 
+	result := convertHex(i, p)
+
+	// Assert 
+	expected := big.NewInt(17)
+
+	if result.Cmp(expected) != 0 {
+		t.Errorf("Expected %d, but result was %d", expected, result)
 	}
 }
